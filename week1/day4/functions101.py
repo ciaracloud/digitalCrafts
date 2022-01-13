605c0c97e5a61edf4bb31e8da05d2bb4540243a0
ciaracloud <ciara_cloud@yahoo.com>
2022-01-13 12:14:38 -0500

    added joe's code to notes

--- week1/day4/functions101.py
+++ week1/day4/functions101.py
@@ -42,5 +42,37 @@ def printCat():
          `-.-' \ )-`( , o o)
      -bf-      `-    \`_`"'-
      """)
-  
+    return
 
+while choice != "n":
+    printCat()
+    choice
+
+def printMenu():
+    return print("""
+    1. print my name
+    2. print my city
+    3. print my favorite food
+    4.quit
+    """)
+
+print("Welcome to Joe's Portfolio!")
+choice = input("Would you like to the see menu y/n \n")
+while choice != "n" and choice != "Q":
+    printMenu()
+    choice = input("What would you like to do? \n")
+    if choice == "1":
+       printName()
+    elif choice == "2":
+       printCity()
+    elif choice == "3":
+       printFood()
+    elif choice == "4":
+       printCity()
+       printName()
+    else:
+        print("Please choose between 1-4!")
+    if choice == "4":
+        print("4")
+    else:
+        print("No valid cho
