deb8e1faeffc0ef713a010493299d23f60864d7a
ciaracloud <ciara_cloud@yahoo.com>
2022-01-14 15:24:52 -0500

    adding initial dictionary practice and 2 exercises

---
+++ week1/day5/dictionaryPractice.py
@@ -0,0 +1,26 @@
+myself = {
+    "name": "Ciara",
+    "city": "Tampas",
+    "height": "5'7",
+    "eye color": "brown",
+    "address": {
+        "streetName": "Hello St",
+        "houseNumber": "123",
+        "zipcode": "12345"
+        },
+    "siblings": {
+        "Brother(s)": "1",
+        "Sister(s)": "5"
+        }
+    "education": {
+        "elementary": ""
+    }
+}
+
+# print(sentence)
+
+# print(f"My name is" {myself["name"]} + ".I am from" + {myself["city"]} ".I am" + {myself["height"]} ".I have" + {myself["eye color"]} ".")
+
+# myself["state"] = "FL"
+
+print(myself["siblings"]["Sister(s)"])
