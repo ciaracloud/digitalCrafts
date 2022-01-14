ea1adba589cf6ca81a58782fa060ed16ff876654
ciaracloud <ciara_cloud@yahoo.com>
2022-01-14 09:32:50 -0500

    day4 HW - #1 and #2 done

---
+++ week1/day4/day4-HW.py
@@ -0,0 +1,19 @@
+# import math
+# number = int(input("what number would you like to use?"))
+# factorialOfNumber = math.factorial(number)
+# print(factorialOfNumber)
+
+
+# word = input("what would you like the word to be?")
+# if word[:] == word[::-1]:
+#     print("This word is a palindrome! :D")
+# else:
+#     print("This word is NOT a palindrome! :(")
+
+
+number = int(input("what number would you like to use?"))
+if number % 1 and number % number:
+    print("This number is a prime number!! ;D")
+else:
+    print("Sorry, this number is NOT a prime number :/")
+
