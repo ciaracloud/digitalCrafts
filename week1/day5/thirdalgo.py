9a08077589031bdcfcb11b5c0e0c1eeec654cc75
ciaracloud <ciara_cloud@yahoo.com>
2022-01-14 14:05:24 -0500

    added santo's version of fig seq

--- week1/day5/thirdalgo.py
+++ week1/day5/thirdalgo.py
@@ -21,3 +21,11 @@ for element in fibSequence:
     else:
         print(fibSequence[choice])
 
+#santo's version (still working on it):
+fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+choice = int(input("Which element of the fibonacci sequence do you want to see? \n"))
+for i in fibonacci:
+    if choice > i:
+        newFib= fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(newFib)
+print(fibonacci[choice])
