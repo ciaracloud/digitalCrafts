0b1f638a94373a8b7b09134b6fe7c890886445ff
ciaracloud <ciara_cloud@yahoo.com>
2022-01-15 10:15:35 -0500

    initial commit - small #1-#4

---
+++ week1/weekendPractice/python101Practice.py
@@ -0,0 +1,18 @@
+#small #1
+# name = input("What is your name?")
+# print("Hello, " + name.capitalize() + "!")
+
+#small #2
+# name = input("What is your name?")
+# print("HELLO, ", name.upper(), "! YOUR NAME HAS", len(name), "LETTERS IN IT! AWESOME!")
+
+#small #3
+# print("Please fill in the blanks below: \n ___(name)___'s favorite subject in school is ___(sibject)___.")
+# name = input("What's your name?")
+# subject = input("What's your favorite subject in school?")
+# print(name.capitalize(), "'s favorite subject in school is", subject.lower(), ".")
+
+#small #4
+daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+userDay = int(input("Which day of the week? (0-6)"))
+print("The day of the week is", daysOfWeek[userDay])
