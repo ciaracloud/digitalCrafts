d04cf45533bd41298eb5b4dcf99f8718e5145579
ciaracloud <ciara_cloud@yahoo.com>
2022-01-19 12:10:40 -0500

    attempted to define user and address

--- week2/day3/classesPractice.py
+++ week2/day3/classesPractice.py
@@ -1,7 +1,12 @@
+addresses = []
+
 class user:
     def __init__(self, first_name, last_name):
         self.firstName = firstName
         self.lastName = lastName
+    def add_address(self,address):
+        addresses = []
+        addresses.append(address)
 
 class address:
     def __init__(self, street, city, state, zip_code):
@@ -9,3 +14,10 @@ class address:
         self.city = city
         self.state = state
         self.zip_code = zip_code
+    def display_addresses(self):
+        print(addresses)
+
+
+ciara = user("Ciara", "Cloud")
+ciara.add_address("123 happy st.")
+print(ciara.add_address)
