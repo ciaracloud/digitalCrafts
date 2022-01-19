0b8842f30c82e3fdb4c25cc2e0f25b7d16a6820f
ciaracloud <ciara_cloud@yahoo.com>
2022-01-19 11:45:36 -0500

    added classesPractice and user/address activity

---
+++ week2/day3/classesPractice.py
@@ -0,0 +1,11 @@
+class user:
+    def __init__(self, first_name, last_name):
+        self.firstName = firstName
+        self.lastName = lastName
+
+class address:
+    def __init__(self, street, city, state, zip_code):
+        self.street = street
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
