bd22fc6f9595ff52887161b3fcdfe20b3c3aa105
ciaracloud <ciara_cloud@yahoo.com>
2022-01-20 11:39:23 -0500

    added while loop for grocery list items

--- week2/day3/groceryApp.py
+++ week2/day3/groceryApp.py
@@ -9,11 +9,24 @@ class ShoppingList:
         for attribute in groceryStore:
             print(attribute,end=" ")
 
-class GroceryItem:
+# class SuperUser(User):
+#     def __init__(self,name,email,password,accessPriviledge):
+#         super().__init__(name,email,password)
+#         self.accessPriviledge = accessPriviledge
+#     def printAdminStatus(self):
+#         print(f"You have {self.accessPriviledge} status")
+
+class GroceryItem(ShoppingList):
     def __init__(self,title,price,quantity):
         self.title = title
         self.price = price
         self.quantity = quantity
+
+# class GroceryItem:
+#     def __init__(self,title,price,quantity):
+#         self.title = title
+#         self.price = price
+#         self.quantity = quantity
 
 while True:
     groceryStoreName = input("What is the name of the grocery store you want to add? ")
@@ -27,8 +40,17 @@ while True:
     if userContinue == "N":
         break
 
+while True:
+    groceryItemName = input("What is the name of the item you need to put on your grocery list? ")
+    groceryItemPrice = input(f"How much is this {groceryItemName}? ")
+    groceryItemQuantity = input(f"How many of {groceryItemName} would you like to purchase? ")
+    userGroceryStoreChoice = input(f"Which grocery store would you like to add {groceryItemName} to? ")
+    # for eachGroceryStore in listOfGroceryStores:
+    #     if eachGroceryStore == userGroceryStoreChoice:
+    userContinue = input("\n Would you like to add another grocery item? If yes, press enter. If no, press 'N'. ")
+    if userContinue == "N":
+        break
+
 # while True:
 #     userGroceryItem = input("What item would you like to add to a shopping list? ")
 #     userShoppingList = input(f"What list would you like to add {userGroceryItem} to? ")
-
-# ShoppingList(groceryStoreName,groceryStoreAddress)
