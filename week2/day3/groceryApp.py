3279afcd848f0d2d0112a309704c1578fa6fbef3
ciaracloud <ciara_cloud@yahoo.com>
2022-01-20 14:36:11 -0500

    joe added changes/solution

--- week2/day3/groceryApp.py
+++ week2/day3/groceryApp.py
@@ -1,32 +1,27 @@
 listOfGroceryStores = []
 
 class ShoppingList:
-    def __init__(self,title,address):
+    def __init__(self,title,address,groceryItems=[]):
         self.title = title
         self.address = address
+        self.groceryItems = groceryItems
     def displayGroceryStore(self):
         groceryStore = [self.title, self.address]         
         for attribute in groceryStore:
             print(attribute,end=" ")
+    def addItemToList(self,groceryItemName):
+        self.groceryItems.append(groceryItemName)
+    def displayCompleteGroceryList(self): 
+        for  store in self.groceryItems:
+            print(self.title)
+            print(f"{store.title} : price: {store.price} quantity: {store.quantity}")
 
-# class SuperUser(User):
-#     def __init__(self,name,email,password,accessPriviledge):
-#         super().__init__(name,email,password)
-#         self.accessPriviledge = accessPriviledge
-#     def printAdminStatus(self):
-#         print(f"You have {self.accessPriviledge} status")
 
-class GroceryItem(ShoppingList):
+class GroceryItem:
     def __init__(self,title,price,quantity):
         self.title = title
         self.price = price
         self.quantity = quantity
-
-# class GroceryItem:
-#     def __init__(self,title,price,quantity):
-#         self.title = title
-#         self.price = price
-#         self.quantity = quantity
 
 while True:
     groceryStoreName = input("What is the name of the grocery store you want to add? ")
@@ -45,8 +40,18 @@ while True:
     groceryItemPrice = input(f"How much is this {groceryItemName}? ")
     groceryItemQuantity = input(f"How many of {groceryItemName} would you like to purchase? ")
     userGroceryStoreChoice = input(f"Which grocery store would you like to add {groceryItemName} to? ")
-    # for eachGroceryStore in listOfGroceryStores:
-    #     if eachGroceryStore == userGroceryStoreChoice:
+    groceryItemName = GroceryItem(groceryItemName,groceryItemPrice,groceryItemQuantity)
+    storeThatMatches = [store for store in listOfGroceryStores if store.title == userGroceryStoreChoice]
+    print(storeThatMatches)
+    for eachGroceryStore in listOfGroceryStores:
+
+        if eachGroceryStore.title == userGroceryStoreChoice:
+            print("true")
+           
+            eachGroceryStore.addItemToList(groceryItemName)
+            # eachGroceryStore.displayCompleteGroceryList()
+    print(f"Here is your grocery list for {userGroceryStoreChoice}:")
+    storeThatMatches[0].displayCompleteGroceryList()
     userContinue = input("\n Would you like to add another grocery item? If yes, press enter. If no, press 'N'. ")
     if userContinue == "N":
         break
