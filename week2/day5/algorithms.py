f098383160d6206975790344f9540f385960eac3
ciaracloud <ciara_cloud@yahoo.com>
2022-01-23 11:11:15 -0500

    added the algorithm to the file

--- week2/day5/algorithms.py
+++ week2/day5/algorithms.py
@@ -1,7 +1,21 @@
-Class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
+# Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+# A subarray is a contiguous part of an array.
+# 9:12
+# Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+# Output: 6
+# Explanation: [4,-1,2,1] has the largest sum = 6.
+
+# Class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
 
 nums = [-2,1,-3,4,-1,2,1,-5,4]
 
 for number in nums:
-    currentLargestSum = nums[0]
+    # currentLargestSum = nums[0] + nums[1]
+    for num in nums:
+       sum = number + num
+       print(sum,end=" ")
+    print()
+
+    
