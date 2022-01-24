a92c625b6e2007bb5b728432d3b31d0b0975c796
ciaracloud <ciara_cloud@yahoo.com>
2022-01-24 15:26:47 -0500

    adds character menu

--- week3/day1/rpgProject.py
+++ week3/day1/rpgProject.py
@@ -2,6 +2,13 @@ line_one = input("Today is the day! You are headed to an interview for your drea
 line_two = input("But wait .... You see Vivian (the girl who has been mean to you since 5th grade)! (Press enter to continue)")
 line_three = input("She is definitely going to try to stop you from making it to the interview by attempting to defeat you in a battle! (Press enter to continue)")
 line_four = input("If you can get past her, you can make it make it to the interview! (Press enter to continue)")
-character_letter = input("""As you approach Vivian, you decide which character you would like to be during battle:
-CHARACTER A:        CHARACTER B:         CHARACTER C:
-""")
+line_five = input("As you approach Vivian, you have to decide which character you would like to be during battle. (Press enter to see the menu of characters to choose from)")
+character_letter = input("""                                       CHARACTER MENU 
+============================================================================================
+            CHARACTER A                CHARACTER B                 CHARACTER C
+            health: 400                health: 200                 health: 500
+            strength: 50               strength: 100               strength: 30
+
+Type the letter of the character you would like to be during battle here: 
+""").upper()
+print(character_letter)
