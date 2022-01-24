6150af2a973cf21324c5d9e3df9391ce231186bf
ciaracloud <ciara_cloud@yahoo.com>
2022-01-24 15:08:18 -0500

    adds beginning lines to game storyline

--- week3/day1/rpgProject.py
+++ week3/day1/rpgProject.py
@@ -1,3 +1,7 @@
-print("Today is the day! You are headed to your interview for your first interior design job! (Press enter to continue)")
-But wait .... You see Vivian (the girl who has been mean to you since 5th grade)! 
-She is definitely going to try to stop you from making it to the interview on time!""")
+line_one = input("Today is the day! You are headed to an interview for your dream job as an interior designer! (Press enter to continue)")
+line_two = input("But wait .... You see Vivian (the girl who has been mean to you since 5th grade)! (Press enter to continue)")
+line_three = input("She is definitely going to try to stop you from making it to the interview by attempting to defeat you in a battle! (Press enter to continue)")
+line_four = input("If you can get past her, you can make it make it to the interview! (Press enter to continue)")
+character_letter = input("""As you approach Vivian, you decide which character you would like to be during battle:
+CHARACTER A:        CHARACTER B:         CHARACTER C:
+""")
