6e6c5cc281476cbf85a956757c01cca17576e79a
ciaracloud <ciara_cloud@yahoo.com>
2022-01-24 14:43:27 -0500

    initial commit

---
+++ week3/day1/rpgProject.py
@@ -0,0 +1,3 @@
+print("Today is the day! You are headed to your interview for your first interior design job! (Press enter to continue)")
+But wait .... You see Vivian (the girl who has been mean to you since 5th grade)! 
+She is definitely going to try to stop you from making it to the interview on time!""")
