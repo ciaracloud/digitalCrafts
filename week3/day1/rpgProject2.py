064dc76f6c907642c49520be25e24b80fef90c67
ciaracloud <ciara_cloud@yahoo.com>
2022-01-25 15:39:21 -0500

    completes attacks for character a

--- week3/day1/rpgProject2.py
+++ week3/day1/rpgProject2.py
@@ -15,6 +15,17 @@ STRENGTH: {self.strength}
 CONFIDENCE: {self.confidence}
 """)
 
+def display_character_menu():
+    print("""                                      
+                                      CHARACTER MENU 
+============================================================================================
+            CHARACTER A                CHARACTER B                 CHARACTER C
+            health: 300                health: 100                 health: 400
+            strength: 50               strength: 100               strength: 30
+            confidence: 0              confidence: 0               confidence: 0
+
+""")
+
 def display_character_a_stats():
     a.display_current_stats()
 
@@ -54,10 +65,10 @@ B. Tickle Vivian
 C. Challenge Vivian to a mini dance battle
 """)
 
-a = Character("A", 400, 50, 0)
-b = Character("B", 200, 100, 0)
-c = Character("C", 500, 30, 0)
-vivian = Character("VIVIAN", 400, 50, 60)
+a = Character("A", 300, 50, 0)
+b = Character("B", 100, 100, 0)
+c = Character("C", 400, 30, 0)
+vivian = Character("VIVIAN", 300, 40, 60)
 
 line_one = input("Today is the day! You are headed to an interview for your dream job as an interior designer! (Press enter to continue)")
 line_two = input("But wait .... You see Vivian (the girl who has been mean to you since 5th grade)! (Press enter to continue)")
@@ -67,19 +78,13 @@ vivian_doesnt_know_name = input("...As you approach Vivian, she pretends she doe
 name_or_run_choice = input("""Vivian: "HEY YOU! What's your name again?" 
 -Type your name to give her your name 
 OR 
--Press 1 to point at a "squirrel" and run past her: """).capitalize()
+-Press 1 to point at a "squirrel" and run past her: """).upper()
 while len(name_or_run_choice) > -1:
     if len(name_or_run_choice) > 0 and name_or_run_choice != "1":
         print_name = input(f"""You: "Come on Vivian! You know that my name is {name_or_run_choice}! Now, let's get this battle over with. I have an interview to get to!" (Press enter to continue)""")
         must_decide_character = input("Now, you have to decide which character you would like to be during battle. (Press enter to see the menu of characters to choose from)")
-        character_letter = input("""                                      CHARACTER MENU 
-============================================================================================
-            CHARACTER A                CHARACTER B                 CHARACTER C
-            health: 400                health: 200                 health: 500
-            strength: 50               strength: 100               strength: 30
-            confidence: 0              confidence: 0               confidence: 0
-
-Type the letter of the character you would like to be during battle here (A, B, or C): """).upper()
+        display_character_menu()
+        character_letter = input("Type the letter of the character you would like to be during battle here (A, B, or C): ").upper()
         while len(character_letter) > -1:
             if character_letter == "A":
                 display_character_letter = input(f"You have chosen to be CHARACTER {character_letter}! (To see their stats press enter)")
@@ -108,19 +113,13 @@ Type the letter of the character you would like to be during battle here (A, B, 
         line_nine = input("Oh no ..it's Vivian again! (Press enter to continue)")
         vivian_doesnt_know_name = input("...As you approach Vivian, she pretends she doesn't know your name to try to make you feel small. (Press enter to continue)") 
         name_or_run_choice = input("""Vivian: "HEY YOU! What's your name again?"
-Type your name here to give her your name: """).capitalize()
+Type your name here to give her your name: """).upper()
         while len(name_or_run_choice) > -1:
             if len(name_or_run_choice) > 0:
                 print_name = input(f"""You: "Come on Vivian! You know that my name is {name_or_run_choice}! Now, let's get this battle over with. I have an interview to get to!" (Press enter to continue)""")
                 must_decide_character = input("Now, you have to decide which character you would like to be during battle. (Press enter to see the menu of characters to choose from)")
-                character_letter = input("""                                      CHARACTER MENU 
-============================================================================================
-            CHARACTER A                CHARACTER B                 CHARACTER C
-            health: 400                health: 200                 health: 500
-            strength: 50               strength: 100               strength: 30
-            confidence: 0              confidence: 0               confidence: 0
-
-Type the letter of the character you would like to be during battle here (A, B, or C): """).upper()
+                display_character_menu()
+                character_letter = input("Type the letter of the character you would like to be during battle here (A, B, or C): ").upper()
                 while len(character_letter) > -1:
                     if character_letter == "A":
                         display_character_letter = input(f"You have chosen to be CHARACTER {character_letter}! (To see their stats press enter)")
@@ -154,27 +153,57 @@ Type the letter of the character you would like to be during battle here (A, B, 
 # elif line_ten == 0 and character_letter == "C":
 #     display_character_c_stats()
 
+vivians_attacks = ["unleashes her pet fire ant on you! OUCH!", "swings her purse at you! Some of her change flew out!", ""]
+
 if character_letter == "A":
     while vivian.health > 0 or a.health > 0:
-        print("Let the battle begin! These are the attack options that you can use against Vivian:")
+        print("These are the attack options that you can use against Vivian:")
         display_attack_options_a()
-        user_attack_choice = input("How would you like to attack Vivian? (Choose A, B, or C)").upper
+        user_attack_choice = input("How would you like to attack Vivian? Choose A, B, or C: ").upper()
         if user_attack_choice == "A":
             a.health -= 50
             vivian.health -= 10
-            print("""You threw your shoe at Vivian! She caught your shoe and threw it back at you!
-Vivian lost 10 health points and you lost 50 health points!
-These are your current stats now:""")
-            display_character_a_stats()
-            
-        break
+            attack_a_continue = input(f"""
+You threw your shoe at Vivian! She caught your shoe and threw it back at you!\nYou lost 50 health points and Vivian lost 10 health points!
+
+YOUR CURRENT HEALTH: {a.health}
+VIVIAN'S CURRENT HEALTH: {vivian.health}
+(Press enter to continue)
+""")
+        elif user_attack_choice == "B":
+            vivian.health -= 50
+            attack_b_continue = input(f"""
+You have decided to tickle Vivian! She HATES being tickled! She lost 50 health points!
+
+YOUR CURRENT HEALTH: {a.health}
+VIVIAN'S CURRENT HEALTH: {vivian.health}
+(Press enter to continue)
+""")
+        elif user_attack_choice == "C":
+            vivian.health -= 50
+            attack_c_continue = input(f"""
+You sang the song that Vivian hates as loud as you can! She lost 50 health points!
+
+YOUR CURRENT HEALTH: {a.health}
+VIVIAN'S CURRENT HEALTH: {vivian.health}
+(Press enter to continue)
+""")
+        else: 
+            display_attack_options_a()
+            user_attack_choice = input("Please choose an attack option from the attack menu").upper()
+        a.health -= 40
+        print(f"""
+Vivian unleashes her pet fire ant on you! OUCH! You lost 40 health points!
+
+YOUR CURRENT HEALTH: {a.health}
+VIVIAN'S CURRENT HEALTH: {vivian.health}""")
 elif character_letter == "B":
     while vivian.health > 0 or b.health > 0:
-        print("Let the battle begin! These are the attack options that you can use against Vivian:")
+        print("These are the attack options that you can use against Vivian:")
         display_attack_options_b()
         break
 elif character_letter == "C":
     while vivian.health > 0 or c.health > 0:
-        print("Let the battle begin! These are the attack options that you can use against Vivian:")
+        print("These are the attack options that you can use against Vivian:")
         display_attack_options_c()
         break
