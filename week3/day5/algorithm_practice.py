1ae3bb1a1d47a7ed4783d7d009ba6f8c9fd95db7
ciaracloud <ciara_cloud@yahoo.com>
2022-01-28 19:48:17 -0500

    adds attempt for algorithm

---
+++ week3/day5/algorithm_practice.py
@@ -0,0 +1,16 @@
+# class Solution {
+#     public:
+#         vector<int> twoSum(vector<int>& numbers, int target) {
+        
+#     }
+# };
+
+numbers = [2,7,11,15]
+target = 9
+
+for number in numbers:
+    counter = 0
+    counter += 1
+    target = 9
+    if number + number[counter] == target:
+        print()
