b4ed6e721d2be3ef3f9e9cb71fbbd6292f48fe55
ciaracloud <ciara_cloud@yahoo.com>
2022-02-07 14:50:38 -0500

    adds loops in JS

--- week5/day1/introToJS.js
+++ week5/day1/introToJS.js
@@ -1,3 +1,48 @@
 function printAUser(user) {
   console.log(user);
 }
+
+// Exercise 1:
+// create a function that prints out the product of 2 numbers
+// return the product of those two numbers
+// store the function return value in a variable
+// print that variable
+
+function productOfNumbers() {
+  return 1 * 2;
+}
+
+let printProduct = productOfNumbers();
+console.log(printProduct);
+
+// Exercise 2:
+// define a function that takes in two arguments
+// those arguments will be numbers or have a type of integer
+// divide the first number by the second number
+// print the result
+
+function quotientOfNumbers(number1, number2) {
+  return number1 / number2;
+}
+
+let printQuotient = quotientOfNumbers();
+console.log(printQuotient);
+
+// OR
+
+function quotientOfNumbers(number1, number2) {
+  console.log(number1 / number2);
+}
+
+// Exercise 3:
+// create a function that takes your first name and last name as arguments
+// find out how to make your first name and last name
+// uppercased
+// return to me the values of your first and last name in this format
+// "I AM {firstName} {lastName}, I am from Tomball"
+
+function fullName1(firstName, lastName) {
+  let upperFirstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+  let upperLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+  console.log(`I AM ${upperFirstName} ${upperLastName}, I am from Tampa`);
+}
---
+++ week5/day1/loopsInJS.js
@@ -0,0 +1,60 @@
+// array (list) []
+
+let studentNames = ["Ciara", "Kiwi"];
+studentNames[0];
+
+// objects (dictionary) {}
+
+let student = {
+    name: "ciara"
+}
+console.log(student["name"])
+console.log(student.name)
+// while loop
+
+while (condition is true) {
+
+}
+
+// for loop (original syntax for arrays)
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+// for...in loop (for objects)
+
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
+
+let accountDetails = {
+    name: "Ciara",
+    city: "Tampa"
+}
+
+for(const key in accountDetails) {
+    console.log(accountDetails[key]);
+}
+
+// for...of loop (for arrays)
+
+for (const iterator of object) {
+    
+}
+
+const houses = ["123 Main St", "1224 Town Square"]
+
+for (const house of houses) {
+    console.log(house)
+}
+
+// .map (used for arrays, loops over array, creates it's own empty array)
+
+const dcInstructors = ["Ciara", "Kiwi", "Taylor"];
+console.log(dcInstructors);
+const dcInstructorsUpper = dcInstructors.map((instructor) => instructor.toUpperCase());
