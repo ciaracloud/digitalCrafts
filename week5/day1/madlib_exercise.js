90cdd4c891c33cc39953da23d381223e699c1c06
ciaracloud <ciara_cloud@yahoo.com>
2022-02-07 20:00:31 -0500

    adds solution for madlib exercise

--- week5/day1/madlib_exercise.js
+++ week5/day1/madlib_exercise.js
@@ -1,4 +1,5 @@
 function madlib(name, subject) {
-  let name = name.charAt(0).toUpperCase + name.slice(1).toLowerCase;
-  console.log(`${name}'s favorite subject in school is ${subject}.`);
+  let nameUpper = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+  let subjectLower = subject.toLowerCase();
+  console.log(`${nameUpper}'s favorite subject in school is ${subjectLower}.`);
 }
