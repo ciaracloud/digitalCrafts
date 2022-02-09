814647ce414daa8e3f4572181192c010a3460299
ciaracloud <ciara_cloud@yahoo.com>
2022-02-08 19:53:15 -0500

    changes file names

--- week5/day1/activity2.js
+++
@@ -1,7 +0,0 @@
-let sumOfNumbers25 = 10 + 20;
-
-if (sumOfNumbers25 > 49 && sumOfNumbers25 < 81) {
-  console.log(65);
-} else {
-  console.log(80);
-}
---
+++ week5/day1/sumOfNumbersExercise.js
@@ -0,0 +1,7 @@
+let sumOfNumbers = 10 + 20;
+
+if (sumOfNumbers > 49 && sumOfNumbers < 81) {
+  console.log(65);
+} else {
+  console.log(80);
+}
