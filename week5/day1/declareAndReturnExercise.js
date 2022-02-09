7a671c57d9023999d4c96c891f2cb2655ed3c733
ciaracloud <ciara_cloud@yahoo.com>
2022-02-08 19:53:31 -0500

    adds declare and return file

---
+++ week5/day1/declareAndReturnExercise.js
@@ -0,0 +1,44 @@
+function lvl1exercise1() {
+  // Declare a variable as null and return it!
+  let x = null;
+  return x;
+}
+
+function lvl1exercise2() {
+  // Declare and instantiate a number and return it
+  let x = 5;
+  return x;
+}
+
+function lvl1exercise3() {
+  // Declare and instantiate a floating point number that is not a whole number and return it
+  let x = 1.5;
+  return x;
+}
+
+function lvl1exercise4() {
+  // Declare and instantiate a string "Hello World!" and return it
+  let x = "Hello World!";
+  return x;
+}
+
+function lvl1exercise5() {
+  // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+  let x = ["Hello World!", 4];
+  return x;
+}
+
+function lvl1exercise6() {
+  // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
+  let x = {
+    key1: "Hello World!",
+    key2: 4,
+  };
+  return x;
+}
+
+function lvl1exercise7() {
+  // Declare and instantiate a boolean value 'false' and return it
+  let x = false;
+  return x;
+}
