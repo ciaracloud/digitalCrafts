68da27481b0b4b30aaf53a434dfb362453d79930
ciaracloud <ciara_cloud@yahoo.com>
2022-02-09 18:19:58 -0500

    adds working calculator without layout

--- week5/day3/calculatorExercise/script.js
+++ week5/day3/calculatorExercise/script.js
@@ -4,14 +4,34 @@ let numbers = document.querySelectorAll(".numbers");
 console.log(numbers);
 let operands = document.querySelectorAll(".operands");
 console.log(operands);
+let clear = document.querySelector(".clear");
+console.log(clear);
 
 let equation = [];
+
+function doTheMath() {
+  operation = equation[1];
+  if (equation.length === 4) {
+    if (operation === "+" && equation[3] === "=") {
+      console.log(equation[0] + equation[2]);
+    } else if (operation === "-" && equation[3] === "=") {
+      console.log(equation[0] - equation[2]);
+    } else if (operation === "/" && equation[3] === "=") {
+      console.log(equation[0] / equation[2]);
+    } else if (operation === "*" && equation[3] === "=") {
+      console.log(equation[0] * equation[2]);
+    }
+    equation = [];
+    console.log("this is the list now:", equation);
+  }
+}
 
 for (i = 0; i < numbers.length; i++) {
   numbers[i].addEventListener("click", (e) => {
     let numberInnerText = parseInt(e.target.innerText);
     equation.push(numberInnerText);
     console.log("this is the current list:", equation);
+    doTheMath();
   });
 }
 
@@ -20,5 +40,11 @@ for (i = 0; i < operands.length; i++) {
     let operandInnerText = e.target.innerText;
     equation.push(operandInnerText);
     console.log("this is the current list:", equation);
+    doTheMath();
   });
 }
+
+clear.addEventListener("click", (e) => {
+  equation = [];
+  console.log("this is the current list", equation);
+});
