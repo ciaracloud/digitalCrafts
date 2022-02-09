5c89b9c0a2ec5b8350f2c9d0a358969aee1c68b9
ciaracloud <ciara_cloud@yahoo.com>
2022-02-08 19:58:34 -0500

    adds strings in JS file

---
+++ week5/day1/stringsInJSExercise.js
@@ -0,0 +1,27 @@
+function lvl3exercise1() {
+  // Create strings "hello" and a "world", return the concatenation of the two
+  return "hello" + "world";
+}
+
+function lvl3exercise2() {
+  // Create a "hello" and a 12, return the concatenation of the two
+  return "hello" + 12;
+}
+
+function lvl3exercise3() {
+  // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
+  let x = 12;
+  return x + "";
+}
+
+function lvl3exercise4() {
+  // Create a "hello world!" string. Return the length of the string
+  let x = "hello world!";
+  return x.length;
+}
+
+function lvl3exercise5() {
+  // Create a "hello world!" string. Return the index of the word "world".
+  let x = "hello world";
+  return x.indexOf("world");
+}
