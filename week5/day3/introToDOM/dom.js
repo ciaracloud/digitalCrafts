6191c1a48cddad4339e149b5070313c2615d4823
ciaracloud <ciara_cloud@yahoo.com>
2022-02-09 12:12:25 -0500

    adds notes

--- week5/day3/introToDOM/dom.js
+++ week5/day3/introToDOM/dom.js
@@ -5,6 +5,7 @@ console.log(document);
 //create variable
 let button = document.getElementById("buttonid");
 console.log(button);
+button.innerText = "New Button";
 
 //getElementsByClass or getElementByClass
 //gets elements by their class name
@@ -16,3 +17,12 @@ console.log(p);
 //give (.) for class name and (#) for id name
 let img = document.querySelector(".image");
 console.log(img);
+
+// in order to change the DOM,
+// follow these steps:
+// 1. select the element you want to change (ex. if you want to change button, select button)
+// 1. select the container
+// 1a. if the element does not exist, you need to create it and then you can select it
+// 2. add the info to the element that you want to change/update
+// 3. append it to the container you selected
+// [SELECT CONTAINER, SELECT ELEMENT(S), CREATE/MODIFY ELEMENT(S), APPEND TO CONTAINER]
