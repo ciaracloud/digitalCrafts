608f1ac1c0dfa72bd9b90eb50d3a963eebe63d28
ciaracloud <ciara_cloud@yahoo.com>
2022-02-08 20:00:58 -0500

    adds array exercise file

---
+++ week5/day1/arrayExercise.js
@@ -0,0 +1,24 @@
+function lvl5exercise1() {
+  // Push the string "hello" into the array and return the array
+  var arr = [1, "adam"];
+  arr.push("Hello");
+  return arr;
+}
+
+function lvl5exercise2() {
+  // Remove the last element from the array and return the array
+  var arr = [1, "adam"];
+  return arr.pop();
+}
+
+function lvl5exercise3() {
+  // Return the length of the array
+  var arr = [1, "adam"];
+  return arr.length;
+}
+
+function lvl5exercise4() {
+  // Return the index of item "adam" in the array
+  var arr = [1, "adam"];
+  return arr.indexOf("adam");
+}
