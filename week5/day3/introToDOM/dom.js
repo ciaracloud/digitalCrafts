be1d0d50c028397c5f2c28df580fbf4b3b01227f
ciaracloud <ciara_cloud@yahoo.com>
2022-02-09 11:39:12 -0500

    adds DOM examples

--- week5/day3/introToDOM/dom.js
+++ week5/day3/introToDOM/dom.js
@@ -1,1 +1,18 @@
 console.log(document);
+
+//getElementById
+//gets an element by their id name
+//create variable
+let button = document.getElementById("buttonid");
+console.log(button);
+
+//getElementsByClass or getElementByClass
+//gets elements by their class name
+let p = document.getElementsByClassName("paragraph");
+console.log(p);
+
+//querySelector
+//gets an elements by class name or id name
+//give (.) for class name and (#) for id name
+let img = document.querySelector(".image");
+console.log(img);
