3afc35dc5c32425def5704cda7de361010672920
ciaracloud <ciara_cloud@yahoo.com>
2022-02-08 22:34:52 -0500

    creates printBannerExercise.js and adds solution

---
+++ week5/day2/printBannerExercise.js
@@ -0,0 +1,13 @@
+//write a function that prints a border around given text with "-"
+
+function printBanner(string) {
+  let str = "";
+  for (i = 0; i < string.length; i++) {
+    str += "-";
+  }
+  console.log(str + "-" + "-");
+  console.log(`-${string}-`);
+  console.log(str + "-" + "-");
+}
+
+printBanner("ciara cloud");
