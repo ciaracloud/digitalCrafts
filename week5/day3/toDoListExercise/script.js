894bfb5938b138ec11f8e536f9951441ae919076
ciaracloud <ciara_cloud@yahoo.com>
2022-02-11 19:47:50 -0500

    creates ability to add new div when box is checked

--- week5/day3/toDoListExercise/script.js
+++ week5/day3/toDoListExercise/script.js
@@ -5,21 +5,28 @@ let pendingTaskContainer = document.querySelector(".pendingTaskContainer");
 let checkbox = document.querySelectorAll(".checkbox");
 let completedTaskContainer = document.querySelector(".completedTaskContainer");
 let tasks = document.querySelectorAll(".tasks");
-
-input.addEventListener("click", (e) => {
-  input.value = "";
-});
+let taskTitles = document.querySelectorAll(".taskTitles");
+let taskContents = document.querySelectorAll(".tasks").childNodes;
 
 taskList = [];
 
-// add.addEventListener("click", (e) => {
-//   taskList.push(input.value);
-//   console.log("this is the task array:", taskList);
-// });
-
 add.addEventListener("click", (e) => {
-  pendingTaskContainer.append(input.value);
-  console.log(pendingTaskContainer);
+  let newDiv = document.createElement("div");
+  newDiv.className = "tasks";
+  let inputTag = document.createElement("input");
+  inputTag.type = "checkbox";
+  inputTag.className = "checkbox";
+  let paragraphTag = document.createElement("p");
+  paragraphTag.innerText = input.value;
+  let buttonTag = document.createElement("button");
+  buttonTag.className = "remove";
+  buttonTag.innerText = "Remove";
+  newDiv.append(inputTag);
+  newDiv.append(paragraphTag);
+  newDiv.append(buttonTag);
+  console.log(newDiv);
+  pendingTaskContainer.append(newDiv);
+  input.value = "";
 });
 
 for (i = 0; i < tasks.length; i++) {
@@ -27,12 +34,35 @@ for (i = 0; i < tasks.length; i++) {
     if (e.target.checked) {
       console.log("box is checked");
       let newDiv = document.createElement("div");
-
-      newDiv.innerText = e.target.innerText;
+      newDiv.className = "tasks";
+      let inputTag = document.createElement("input");
+      inputTag.type = "checkbox";
+      inputTag.className = "checkbox";
+      let paragraphTag = document.createElement("p");
+      paragraphTag.innerText = "this is a completed task";
+      let buttonTag = document.createElement("button");
+      buttonTag.className = "remove";
+      buttonTag.innerText = "Remove";
+      newDiv.append(inputTag);
+      newDiv.append(paragraphTag);
+      newDiv.append(buttonTag);
       completedTaskContainer.append(newDiv);
     } else {
-      console.log("box is not checked");
-      pendingTaskContainer.append(e);
+      console.log("box is checked");
+      let newDiv = document.createElement("div");
+      newDiv.className = "tasks";
+      let inputTag = document.createElement("input");
+      inputTag.type = "checkbox";
+      inputTag.className = "checkbox";
+      let paragraphTag = document.createElement("p");
+      paragraphTag.innerText = "this is a pending task";
+      let buttonTag = document.createElement("button");
+      buttonTag.className = "remove";
+      buttonTag.innerText = "Remove";
+      newDiv.append(inputTag);
+      newDiv.append(paragraphTag);
+      newDiv.append(buttonTag);
+      pendingTaskContainer.append(newDiv);
     }
   });
 }
