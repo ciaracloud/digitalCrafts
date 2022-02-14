c3a1a126f6816d46e31dad641074225849f8d3bb
ciaracloud <ciara_cloud@yahoo.com>
2022-02-14 11:18:05 -0500

    adds ES6 loop notes

--- week6/day1/ES6/script.js
+++ week6/day1/ES6/script.js
@@ -15,7 +15,8 @@ function printHi() {
 
 //------------------------- {FUNCTIONS IN ES6} -----------------------------
 
-// old way to write a function (function can be redifined)
+// first way to write a function (function can be redefined)
+// GETS ACCESS TO 'this'
 function printHi() {
   let hi = "hi";
   console.log(hi);
@@ -23,8 +24,53 @@ function printHi() {
 
 printHi = 0;
 
-// new way adds const (so function can't be redefined)
+// second way to write a function (called arrow function)
+// adds const (function can't be redefined)
+// DOESN'T GET ACCES TO 'this'
 const printHi = () => {
   let hi = "hi";
   console.log(hi);
 };
+
+//-----------------------------{SETS & MAPS}--------------------------------------
+
+// declare a set - can't have duplicates in a set
+// set can't have duplicate keys or values
+const names = new Set();
+console.log(names);
+
+// declare a map
+const mapName = new Map();
+console.log(mapName);
+
+//----------------------------{DESTRUCTURING}-------------------------------------
+
+const Kiwi = {
+  name: "kiwi",
+  age: "2",
+  height: "1",
+};
+
+let name = Kiwi.name;
+let age = Kiwi.age;
+let height = Kiwi.height;
+
+// syntactic sugar - ways to define, must use the name of the keys
+// use a for loop to get access to multiple names, ages, heights
+const { name, age, height } = Kiwi;
+
+//----------------------------{LOOPS}---------------------------------------------
+
+// for - used for arrays
+for (let index = 0; index < li; index++) {}
+
+// for of - used for arrays
+for (let student of array) {
+}
+
+// for each - used for arrays
+listName.forEach((element) => console.log(element));
+
+// for in - used for objects
+for (let student in object) {
+}
