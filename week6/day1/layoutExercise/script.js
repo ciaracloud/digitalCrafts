88ea67abf8170d991b6b0577b3270a772164a143
ciaracloud <ciara_cloud@yahoo.com>
2022-02-14 18:52:17 -0500

    adds layout and styling

--- week6/day1/layoutExercise/script.js
+++ week6/day1/layoutExercise/script.js
@@ -7,17 +7,15 @@ const movie = {
   Genre: "Comedy",
   Director: "Mark Waters",
   Writer: "Rosalind Wiseman, Tina Fey",
-  Actors: "Lindsay Lohan, Jonathan Bennett, Rachel McAdams",
-  Plot: "Cady Heron is a hit with The Plastics, the A-list girl clique at her new school, until she makes the mistake of falling for Aaron Samuels, the ex-boyfriend of alpha Plastic Regina George.",
+  Actors: ["Lindsay Lohan", "Jonathan Bennett", "Rachel McAdams"],
+  Plot: `Cady Heron is a hit with The Plastics, the A-list girl clique at her new school, until she makes the mistake of falling for Aaron Samuels, the ex-boyfriend of alpha Plastic Regina George.`,
   Language: "English, German, Vietnamese, Swahili",
   Country: "United States, Canada",
   Awards: "7 wins & 25 nominations",
   Poster1:
-    "https://thumbnails.cbsig.net/_x/w1200/CBS_Production_Entertainment_VMS/2021/07/16/1922594371964/MG_SAlone_16_9_1920x1080_768754_1920x1080.jpg",
-  Poster2:
     "https://play-lh.googleusercontent.com/FaBWrOOZBR3NcTCNKsUnKG3zzIIIYech-dwsCgw0XdLsN4WIM68k9pnh6YhTaVLsV9UL=w400-h600-rw",
-  Image:
-    "http://1.bp.blogspot.com/-SANt9KueKHQ/UWrtg0m6FRI/AAAAAAAAJtk/J-iQudQ_l4I/s1600/grils+wallpapers++12.jpg",
+  Image1:
+    "https://imgix.bustle.com/nylon/18542152/origin.jpg?w=1200&h=1000&fit=crop&crop=faces&auto=format%2Ccompress",
   Ratings: [
     {
       Source: "Internet Movie Database",
@@ -43,3 +41,44 @@ const movie = {
   Website: "N/A",
   Response: "True",
 };
+
+const container = document.querySelector(".container");
+
+const { Title, Year, Genre, Rated, Actors, Plot, Poster1, Image1 } = movie;
+const titleElement = document.createElement("h1");
+titleElement.innerText = Title;
+const posterElement = document.createElement("img");
+posterElement.src = Poster1;
+posterElement.className = "poster";
+const imageElement = document.createElement("img");
+imageElement.src = Image1;
+imageElement.className = "image";
+const plotElement = document.createElement("p");
+plotElement.innerText = Plot;
+plotElement.className = "plot";
+const castContainer = document.createElement("div");
+castContainer.className = "castContainer";
+const castElement = document.createElement("p");
+castElement.innerText = `Starring ${Actors[0]}, ${Actors[1]}, ${Actors[2]}`;
+const genreElement = document.createElement("p");
+genreElement.innerText = Genre;
+genreElement.className = "genre";
+const ratedElement = document.createElement("p");
+ratedElement.innerText = Rated;
+const yearElement = document.createElement("p");
+yearElement.innerText = Year;
+const contentContainer = document.createElement("div");
+const yearRatedContainer = document.createElement("div");
+yearRatedContainer.className = "yearRated";
+const posterContentContainer = document.createElement("div");
+posterContentContainer.className = "posterContent";
+posterContentContainer.append(posterElement, contentContainer);
+yearRatedContainer.append(yearElement, ratedElement);
+castContainer.append(castElement);
+container.append(imageElement, titleElement, posterContentContainer);
+contentContainer.append(
+  yearRatedContainer,
+  genreElement,
+  plotElement,
+  castContainer
+);
