67252b95951aeda77b23314045143c55f8146620
ciaracloud <ciara_cloud@yahoo.com>
2022-02-14 10:28:44 -0500

    creates files and adds declaring variable notes

---
+++ week6/day1/ES6/script.js
@@ -0,0 +1,16 @@
+// ES6
+
+// DECLARING VARIABLEs
+// block scope
+// this variable is defined at a global level
+
+// use const for global
+const variableName = { name: "Ciara" };
+
+// use let for local
+function printHi() {
+  let hi = "hi";
+  console.log(hi);
+}
+
+// FUNCTION IN ES6
