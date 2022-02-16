e4464c2eed7da77d41aeefb8e1647aeb5329dd9d
ciaracloud <ciara_cloud@yahoo.com>
2022-02-16 10:54:34 -0500

    adds title and poster to webpage

--- week6/day2/apiExercise/script.js
+++ week6/day2/apiExercise/script.js
@@ -1,7 +1,32 @@
 const form = document.querySelector("form");
-const movie = document.querySelector(".movie")
+const inputMovie = document.querySelector(".inputMovie");
+const movieContainer = document.querySelector(".movieContainer");
 
-const checkMovieInput = (movie) => {
-    if (movie.)
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  movieContainer.innerHTML = "";
+  console.log(inputMovie.value);
+  const getMovie = async () => {
+    const movie = await fetch(
+      `http://www.omdbapi.com/?apikey=c2b8d3c5&t=${inputMovie.value}&`
+    );
+    const movieJson = await movie.json();
+    console.log(movieJson);
+    const movieName = document.createElement("h3");
+    movieName.innerText = movieJson.Title;
+    console.log(movieName);
+    movieContainer.append(movieName);
+    const movieImg = document.createElement("img");
+    movieImg.src = movieJson.Poster;
+    movieContainer.append(movieImg);
+    inputMovie.value = "";
+  };
+  getMovie();
+});
 
-}
+// const testFunction = async () => {
+//   const test = await fetch(`http://www.omdbapi.com/?apikey=c2b8d3c5&s=Home&`);
+//   const testJson = await test.json();
+//   console.log(testJson);
+// };
+// testFunction();
