bc941d4cf6d439fad248e413110b1be1e4a9dee9
ciaracloud <ciara_cloud@yahoo.com>
2022-02-16 14:22:22 -0500

    adds basic layout for weather app

---
+++ week6/day3/WEATHER/script.js
@@ -0,0 +1,11 @@
+const input = document.querySelector("input");
+const button = document.querySelector(".btn.btn-info");
+
+button.addEventListener("click", () => {
+  searchWeather();
+});
+
+const searchWeather = async () => {
+  console.log(input.value);
+  const url = 
+};
