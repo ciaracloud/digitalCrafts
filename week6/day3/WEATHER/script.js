ebf964750ea2d0c764c86690afb4fadfdc956ce1
ciaracloud <ciara_cloud@yahoo.com>
2022-02-16 19:28:18 -0500

    adds url for weather

--- week6/day3/WEATHER/script.js
+++ week6/day3/WEATHER/script.js
@@ -1,5 +1,6 @@
 const input = document.querySelector("input");
 const button = document.querySelector(".btn.btn-info");
+const weatherContainer = document.querySelector(".weatherContainer");
 
 button.addEventListener("click", () => {
   searchWeather();
@@ -7,5 +8,8 @@ button.addEventListener("click", () => {
 
 const searchWeather = async () => {
   console.log(input.value);
-  const url = 
+  const url = `http://api.openweathermap.org/data/2.5/weather?appid=05407dc4254bea406be637af43796a6a&zip=${input.value}`;
+  const weather = await fetch(url);
+  const weatherJson = await weather.json();
+  console.log(weatherJson);
 };
