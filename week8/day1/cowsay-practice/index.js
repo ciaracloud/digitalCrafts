6e8947d64660c02042f2c9f7613f86dca7c0082c
ciaracloud <ciara_cloud@yahoo.com>
2022-02-28 17:25:19 -0500

    adds cowsay practice solution

--- week8/day1/cowsay-practice/index.js
+++ week8/day1/cowsay-practice/index.js
@@ -1,3 +1,31 @@
+const express = require("express");
+const app = express();
+const PORT = 3003;
+app.listen(PORT, console.log(`listening on port http://localhost:${PORT}`));
 const cowsay = require("cowsay");
 
-console.log(cowsay.say({ text: " " }));
+// console.log(cowsay.say({ text: " " }));
+
+app.post("/sendCowUserName", (req, res) => {
+  const cowsayText1 = cowsay.say({ text: "Adding cow user" });
+  res.send(cowsayText1);
+});
+
+app.post("/sendCowAppointment", (req, res) => {
+  const cowsayText2 = cowsay.say({ text: "Setting up cow appointment" });
+  res.send(cowsayText2);
+});
+
+app.get("/getCowUserName", (req, res) => {
+  const cowsayText3 = cowsay.say({ text: "Adding cow user" });
+  res.send(cowsayText3);
+});
+
+app.get("/getCowAppointment", (req, res) => {
+  const cowsayText2 = cowsay.say({ text: "Setting up cow appointment" });
+  res.send(cowsayText2);
+});
+
+app.delete("/deleteCowUser", (req, res) => {
+  res.send(console.log("deleted user"));
+});
