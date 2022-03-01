ba3baad8ecaa99235ba43b92746a6ba2a40a068d
ciaracloud <ciara_cloud@yahoo.com>
2022-03-01 14:21:44 -0500

    adds song titles

--- week8/day2/music-app/musicDB.js
+++ week8/day2/music-app/musicDB.js
@@ -2,13 +2,14 @@ let songs = [
   {
     name: "The Carter",
     publishDate: "2004",
-    imgURL: "",
-    songTitles: [],
+    imgURL: "https://upload.wikimedia.org/wikipedia/en/c/c8/CarterIII.jpg",
+    songTitles: ["Walk In", "We Don't", "Go DJ", "On My Own"],
   },
   {
     name: "Free Weezy",
     publishDate: "2015",
-    imgURL: "",
-    songTitles: [],
+    imgURL:
+      "https://townsquare.media/site/812/files/2015/02/free_weezy.jpg?w=1200&h=0&zc=1&s=0&a=t&q=89",
+    songTitles: ["Glory", "My Heart Races On", "Psycho", "I Feel Good"],
   },
 ];
