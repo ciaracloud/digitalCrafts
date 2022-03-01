c276d924a443ddbab4ddb703c7b8f9e8a62b5c31
ciaracloud <ciara_cloud@yahoo.com>
2022-03-01 14:16:11 -0500

    adds songs to musicDB

---
+++ week8/day2/music-app/musicDB.js
@@ -0,0 +1,14 @@
+let songs = [
+  {
+    name: "The Carter",
+    publishDate: "2004",
+    imgURL: "",
+    songTitles: [],
+  },
+  {
+    name: "Free Weezy",
+    publishDate: "2015",
+    imgURL: "",
+    songTitles: [],
+  },
+];
