bfdf07b84f6fbb77eb5b879ec4cc33b45435cc30
ciaracloud <ciara_cloud@yahoo.com>
2022-03-02 15:28:16 -0500

    adds practiceExercise files

---
+++ week8/day3/practiceExercise/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+const ejs = require("ejs");
+const path = require("path");
+
+const urlList = [
+  "https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  "https://images.unsplash.com/photo-1611267254323-4db7b39c732c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80",
+  "https://images.unsplash.com/photo-1533743983669-94fa5c4338ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1584&q=80",
+  "https://images.unsplash.com/photo-1587996833651-06a23343b15d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80",
+  "https://images.unsplash.com/photo-1584102026323-27d7a9abd970?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+  "https://images.unsplash.com/photo-1516750105099-4b8a83e217ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+];
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.render("pages/homePage");
+});
+
+app.get("/about", (req, res) => {
+  res.render("pages/aboutPage");
+});
+
+app.listen(PORT, console.log(`listening on http://localhost${PORT}`));
