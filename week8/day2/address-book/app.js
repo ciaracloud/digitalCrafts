ab8c5d2dabefa67aade4a351f725a89dc26a2c15
ciaracloud <ciara_cloud@yahoo.com>
2022-03-02 10:25:23 -0500

    adds files/creates server for practice

---
+++ week8/day2/address-book/app.js
@@ -0,0 +1,16 @@
+const friends = [
+  {
+    name: "Taylor",
+    age: 24,
+  },
+  {
+    name: "Kiwi",
+    age: 1,
+  },
+  {
+    name: "Jalin",
+    age: 15,
+  },
+];
+
+module.exports = friends;
--- week8/day3/index.js
+++ week8/day3/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const friends = require("./app");
 
 app.listen(PORT, console.log(`listening on http://localhost:${PORT}`));
