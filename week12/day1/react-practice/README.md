4981f4bd864cd0f4286b5b35d06424d88ea639f7
ciaracloud <ciara_cloud@yahoo.com>
2022-03-28 11:19:43 -0400

    creates react app

---
+++ week12/day1/react-practice/README.md
@@ -0,0 +1,1 @@
+## Classes React Project
---
+++ week12/day1/react-practice/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import ClassComponent from "./components/Class";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App js</h1>
+      <ClassComponent />
+    </div>
+  );
+}
+
+export default App;
