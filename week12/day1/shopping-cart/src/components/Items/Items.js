2c8e5854c1760d09dbdc6209641c3a3fe2cfd7af
ciaracloud <ciara_cloud@yahoo.com>
2022-03-31 19:15:33 -0400

    adds routes

---
+++ week12/day1/shopping-cart/src/components/Items/Items.js
@@ -0,0 +1,7 @@
+import "./Items.css";
+
+import React from "react";
+
+export default function Items() {
+  return <div>Items</div>;
+}
---
+++ week12/day1/shopping-cart/src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import "./Navbar.css";
+
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <a href="">Items</a>
+      <a href="">Cart</a>
+    </div>
+  );
+}
